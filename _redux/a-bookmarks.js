2c115995fb718a50c159b2958b71239747473209
caaker <caaker.0@gmail.com>
2025-01-25 17:12:35 -0600

    Update a-bookmarks.js

--- _redux/a-bookmarks.js
+++ _redux/a-bookmarks.js
@@ -2,16 +2,13 @@ const Bookmarks = (state = {bookmarks: false}, action) => {
   let newState = { ...state };
   switch(action.type) {
 
-    // called by initial page load by Data component
+    // called on initial page load by Data component
     case 'initializeBookmarks':
       newState.bookmarks = action.bookmarks;
       return newState;
-
   }
 
-  // no change, return the previous state
   return state;
-
 };
 
 export default {
