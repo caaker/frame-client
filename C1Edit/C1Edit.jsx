68184b253feadc5e4b3387b147764e1b46a5fc02
caaker <caaker.0@gmail.com>
2025-02-02 17:24:03 -0600

    Update C1Edit.jsx

--- C1Edit/C1Edit.jsx
+++ C1Edit/C1Edit.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import SVGEdit from '../C0Vectors/SVGEdit.jsx';
 
-export default (props) => {
+export default function C1Edit({ article }) {
   const dispatch = useDispatch();
   function editClicked() {
-    dispatch({type: 'toggleModalOn', config: true});
-    dispatch({type: 'setArticleForm', data: props.article});
+    dispatch({ type: 'toggleModalOn', config: true });
+    dispatch({ type: 'setArticleForm', data: article });
   }
-  return <SVGEdit className='medd_edit' onClick={event => editClicked(event)} />
-};
+  return <SVGEdit className="medd_edit" onClick={editClicked} />;
+}
