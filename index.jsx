d1fbcf6da2b0be778b0e85cc1391fafa7d02479c
chris <caaker.0@gmail.com>
2025-02-18 16:22:04 -0600

    auto-commit

--- index.jsx
+++ index.jsx
@@ -8,9 +8,8 @@ import F1           from './F1/F1.jsx';
 import './F1Customize/class.Header.js';
 import './F1Customize/class.GlobalCSS.js';
 
-console.logD('DEBUG: L0 : index.jsx ---->');
+console.logD('DEBUG: L0 : index.jsx -------->');
 
-// updated on move to react / react-dom 18.2
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
   <Provider store={store}>
