0d736da48cc017d90a9bdd311d2af983aa5c42d2
chris <caaker.0@gmail.com>
2025-02-23 20:50:19 -0600

    auto-commit

--- C1Favicon/C1Favicon.jsx
+++ C1Favicon/C1Favicon.jsx
@@ -4,7 +4,8 @@ import React from 'react';
 
 export default function C1Favicon (props) {
 
-  // does not fire on a "404 error"
+  // does not fire on a "404 error", but fires on "(failed)net::ERR_NETWORK_CHANGED"
+  // characterize by disconnecting and reviewing the console
   function errorHandler (event) {
     event.target.src = '/images/medical-symbol.jpeg';
     console.logD('DEBUG: Google Favicon API: failed', 'red');
