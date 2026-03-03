4676491a3a1f98f123a1f60c9323642bde7c5c38
caaker <caaker.0@gmail.com>
2026-03-03 16:39:37 -0600

    Update favicon logic to use dynamic parameter

--- F1Customize/class.Header.js
+++ F1Customize/class.Header.js
@@ -9,10 +9,16 @@ class Header {
   }
 
   setFavicon() {
+
+    // Remove existing favicon if present
+    const existing = document.querySelector("link[rel='shortcut icon']");
+    if (existing) existing.remove();
+
+    // Add new dynamic favicon
     const link = document.createElement('link');
     link.type = 'image/x-icon';
     link.rel = 'shortcut icon';
-    link.href = config.favicon + '?favicon=true';
+    link.href = config.favicon + '?favicon_dynamic=true';
     document.head.appendChild(link);
   }
 
