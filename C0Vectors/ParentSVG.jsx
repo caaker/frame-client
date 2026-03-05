32c9722c19d0940be5760885561b5b04692c1160
caaker <caaker.0@gmail.com>
2026-03-05 12:24:23 -0600

    Update ParentSVG.jsx

--- C0Vectors/ParentSVG.jsx
+++ C0Vectors/ParentSVG.jsx
@@ -1,16 +1,15 @@
 import React           from 'react';
 import                      './ParentSVG.css';
 
-const ParentSVG = ({ path, fontSize = '25px', onClick = () => {}, id = '', className = '' }) => {
+const ParentSVG = ({ path, fontSize = '25px', onClick = () => {}, id, className = '' }) => {
   const computedClassName = className ? `css-zicon ${className}` : 'css-zicon';
   return (
     <svg
-      id={id}
+      {...(id && { id })}      
       className={computedClassName}
       fontSize={fontSize}
       onClick={onClick}
       viewBox="0 0 24 24"
-      aria-hidden="true"
     >
       {path}
     </svg>
@@ -20,5 +19,7 @@ const ParentSVG = ({ path, fontSize = '25px', onClick = () => {}, id = '', class
 export default ParentSVG;
 
 /*
-removed:      focusable="false"
+removed -> focusable="false" -> legacy IE code
+removed -> aria-hidden="true" as it should be accessible to screen readers
+fixed default id issue for best practice
 */
