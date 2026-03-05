305eaee75747a19d0576c6572a8e590927ae35b8
caaker <caaker.0@gmail.com>
2026-03-05 15:16:53 -0600

    Update daily_midnight.js

--- _dist/daily_midnight.js
+++ _dist/daily_midnight.js
@@ -10,7 +10,7 @@ const controller = new AbortController();
 const browser = {
   signal: controller.signal,
   headers: {
-    // The "Agent" - mimics a modern Chrome browser
+    // this agent mimics a modern Chrome browser, required when run from github actions
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
     'Accept': 'application/json',
     'Cache-Control': 'no-cache'
