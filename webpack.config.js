0bdf659060d18232879931abfa85f29ea8bc2ae6
caaker <caaker.0@gmail.com>
2026-03-09 18:11:26 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ const jsx = {
   use: {
     loader: 'babel-loader',
     options: {
-      presets: ['@babel/preset-env', '@babel/preset-react']
+      presets: ['@babel/preset-env', ['@babel/preset-react', { runtime: 'automatic' }]]
     }
   }
 };
