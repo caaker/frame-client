d72ea328a2ed1d29831c8a637e954881dbda3a22
caaker <caaker.0@gmail.com>
2026-03-10 00:09:32 +0000

    comments added

--- _dist/download.js
+++ _dist/download.js
@@ -1,3 +1,6 @@
+// Downloads images from cache.json, saves to images-articles/ with slugified titles
+// Utilizes github lfs to store images in the repo without bloating it
+
 const https = require("https");
 const fs = require("fs");
 const path = require("path");
@@ -8,6 +11,8 @@ fs.mkdirSync(outDir);
 
 items.forEach(({ title, image }) => {
   const ext = path.extname(image);
+
+  // only keep letters and numbers, replace the rest with dashes
   const filename = title.toLowerCase().replace(/[^a-z0-9]+/g, "-") + ext;
   const dest = path.join(outDir, filename);
   https.get(image, (res) => res.pipe(fs.createWriteStream(dest)));
