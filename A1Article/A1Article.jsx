8b340f718d8e0f140db3f3e23f3e4fc40cd79a2d
caaker <caaker.0@gmail.com>
2026-03-10 14:07:28 -0500

    Update A1Article.jsx

--- A1Article/A1Article.jsx
+++ A1Article/A1Article.jsx
@@ -7,15 +7,13 @@ import                        './A1Article.css';
 
 export default function A1Article () {
   console.logD('DEBUG: L3 : F1-Page-A1Article ');
+  
   useEffect(() => {
     arc.scrollToHash(0);
   });
 
   let articles = useSelector((state) => state.Articles.articles);
   let search = useSelector((state) => state.SearchInput.current);
-
-  console.logD('DEBUG:', 'red');
-  console.logD(articles);
 
   function makeArticles() {
     if(articles) {
