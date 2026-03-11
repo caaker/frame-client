ad9077a78bae0a2050e519fee921a5e472affb03
caaker <caaker.0@gmail.com>
2026-03-11 16:40:58 -0700

    Update download_images.js

--- download_images.js
+++ download_images.js
@@ -1,21 +1,19 @@
-
-// Downloads images from cache.txt, saves to images-articles/ with slugified titles
-// Utilizes github lfs to store image references in the repo and not the entire file
-
 const https = require("https");
 const fs = require("fs");
 const path = require("path");
 
-const items = JSON.parse(fs.readFileSync("cache.txt", "utf8"));
-const outDir = "images-articles";
+const inputPath = path.join("_dist", "cache.txt");
+const items = JSON.parse(fs.readFileSync(inputPath, "utf8"));
 
-fs.mkdirSync('images-articles', { recursive: true });
+fs.mkdirSync("_dist/images-articles", { recursive: true });
 
+// loop through each json entry
 items.forEach(({ title, image }) => {
-  const ext = path.extname(image);
 
-  // only keep letters and numbers, replace the rest with dashes
-  const filename = title.toLowerCase().replace(/[^a-z0-9]+/g, "-") + ext;
-  const dest = path.join(outDir, filename);
-  https.get(image, (res) => res.pipe(fs.createWriteStream(dest)));
+  // only keep letters and numbers and replace the rest with dashes for the filename
+  const filename = title.toLowerCase().replace(/[^a-z0-9]+/g, "-") + path.extname(image);
+  const outputPath = path.join("_dist/images-articles", filename);
+  
+  // get the image and save it to the output
+  https.get(image, (res) => res.pipe(fs.createWriteStream(outputPath)));
 });
