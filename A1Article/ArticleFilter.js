fbf2e9c66dab330ac5496ca2262963ed78a7411a
caaker <caaker.0@gmail.com>
2026-03-15 14:28:20 -0500

    Update ArticleFilter.js

--- A1Article/ArticleFilter.js
+++ A1Article/ArticleFilter.js
@@ -1,15 +1,11 @@
-export default function Search (article, search_string) {
-  if( article.title.toLowerCase().search(search_string.toLowerCase()) !== -1 ) {
-    return true;
-  }
-  if( article.summary.toLowerCase().search(search_string.toLowerCase()) !== -1 ) {
-    return true;
-  }
-  if( article.tag.toLowerCase().search(search_string.toLowerCase()) !== -1 ) {
-    return true;
-  }
-  if( article.domain.toLowerCase().search(search_string.toLowerCase()) !== -1 ) {
-    return true;
-  }
+export default function Search(article, search_string) {
+
+  // nullish coalescing operator
+  const query = (search_string ?? '').toLowerCase();
+  if (!article || !query) return false;
+  if (article.title.toLowerCase().includes(query)) return true;
+  if (article.summary.toLowerCase().includes(query)) return true;
+  if (article.tag.toLowerCase().includes(query)) return true;
+  if (article.domain.toLowerCase().includes(query)) return true;
   return false;
 }
