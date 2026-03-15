c7a406f363b24ac347ba7a930f547e2553616f76
caaker <caaker.0@gmail.com>
2026-03-15 15:39:42 -0500

    Update Article.jsx

--- A1Article/Article.jsx
+++ A1Article/Article.jsx
@@ -10,32 +10,46 @@ export default function Article ({ article }) {
   
   return (
     <article id={hash} className='__article'>
-      <a className="article_link" href={article.link} target="_blank">
 
+      <a 
+        className="article_link" 
+        href={article.link} 
+        target="_blank"
+        rel="noreferrer"
+      >
         <img
           className="article_link_image"
+          rel="noreferrer"
           src={article.image}
+          alt={article.title}
           onError={(error) => {
             error.currentTarget.src = image_not_found;
+            console.logD('DEBUG: L4 : F1-Page-A1Article-Article : Image not Found ', 'red');
+
+            // prevents infinite loops if the fallback image fails
+            e.currentTarget.onerror = null;
           }}
         />
-
       </a>
+
       <p className="article_heading">
         <a className="article_heading_link" href={article.link} target="_blank">{article.title}</a>
       </p>
+
       <p className="article_summary">
         {article.summary}
       </p>
+
       <p className="article_tag">
         {article.tag}
       </p>
+
       <p className="article_domain">
         {article.domain}
       </p>
 
       <ArticleBar article={article}/>
+
     </article>
   );
 };
-
