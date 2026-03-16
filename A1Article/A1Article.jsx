a9d1f1a86071f72f9d97863bdf498712d6595da5
caaker <caaker.0@gmail.com>
2026-03-16 09:15:43 -0500

    Update A1Article.jsx

--- A1Article/A1Article.jsx
+++ A1Article/A1Article.jsx
@@ -6,7 +6,7 @@ import ArticleFilter          from './ArticleFilter.js';
 import                        './A1Article.css';
 
 export default function A1Article () {
-  console.logD('DEBUG: L3 : F1-Page-A1Article ');
+  console.logD('DEBUG: L3 : F1-Page-A1Article : Hello World');
   
   useEffect(() => {
     arc.scrollToHash(0);
