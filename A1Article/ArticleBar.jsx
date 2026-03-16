1167bbd456c7cf9c7e7b4f295ea96f1260a05a16
caaker <caaker.0@gmail.com>
2026-03-16 11:54:54 -0500

    Update ArticleBar.jsx

--- A1Article/ArticleBar.jsx
+++ A1Article/ArticleBar.jsx
@@ -1,8 +1,15 @@
 import { useSelector }  from    'react-redux';
+
+// right
 import C1Copy           from    '../C1Copy/C1Copy.jsx';
 import C1Edit           from    '../C1Edit/C1Edit.jsx';
 import C1Delete         from    '../C1Delete/C1Delete.jsx';
+import C1Flip           from    '../C1Flip/C1Flip.jsx';
+
+// left
 import C1Favicon        from    '../C1Favicon/C1Favicon.jsx';
+
+// css
 import                          './ArticleBar.css';
 
 export default function ArticleBar({article}) {
@@ -13,6 +20,8 @@ export default function ArticleBar({article}) {
   return (
     <div className='article_bar' >
       <C1Favicon className='c1_favicon' domain={article.domain}/>
+      
+      <C1Flip article={article}   admin={admin}/>
       <C1Copy title={article.title} />
       <div className='article_bar_admin'>
         <C1Edit   article={article}   admin={admin}/>
