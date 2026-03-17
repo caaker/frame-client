28e625484cbaa2381294049d143b87cc05f6ba94
caaker <caaker.0@gmail.com>
2026-03-17 12:29:19 -0500

    removed contact

--- F1Footer/F1Footer.jsx
+++ F1Footer/F1Footer.jsx
@@ -5,14 +5,12 @@ import { useSelector } from 'react-redux';
 export default function F1Footer({config})  {
   console.logD('DEBUG: L2 : F1-Footer ');
 
-  const mailer = `mailto:${ config.email }?subject=${ config.subject }`;
   const page = useSelector((state)=>state.MenuPage.current);
   const style = (page === 'User') ? 'footer_show ' : 'footer_hide';
 
   return (
     <div id="footer_hold" className={style}>
       <div id="footer_inner" className='footer_style_90'>
-        <a className="footer_item" href={mailer}>contact</a>
         <a className="footer_item" target="_blank" href={config.github}>github</a>
       </div>
     </div>
