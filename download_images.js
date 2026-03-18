65a504cc3d019aeea1b3618c651701af27e29598
caaker <caaker.0@gmail.com>
2026-03-18 13:31:52 -0500

    Update download_images.js

--- download_images.js
+++ download_images.js
@@ -1,11 +1,17 @@
+// requires node 22+
 const fs = require("fs/promises");
 const path = require("path");
 const inputPath = path.join("_public-vite", "cache.txt");
-const items = JSON.parse(await fs.readFile(inputPath, "utf8"));
+const items = JSON.parse( await fs.readFile(inputPath, "utf8"));
 
-items.forEach(async ({ title, image }) => {
-  const filename = title.toLowerCase().replace(/[^a-z0-9]+/g, "-") + path.extname(image);
-  const outputPath = path.join("_public-vite/images-articles", filename);
-  const res = await fetch(image);
-  if (res.ok) await fs.writeFile(outputPath, res.body);
-});
+for (const { title, image } of items) {
+  try {
+    const filename = title.toLowerCase().replace(/[^a-z0-9]+/g, "-") + path.extname(image);
+    const outputPath = path.join("_public-vite/images-articles", filename);   
+    const res = await fetch(image);
+    if (!res.ok) throw new Error(`HTTP ${res.status}`);
+    await fs.writeFile(outputPath, res.body);
+  } catch (err) {
+    console.error(`Error at: "${title}": ${err.message}`);
+  }
+}
