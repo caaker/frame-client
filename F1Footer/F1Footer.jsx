2f6ab5999f67ebb07e33ead7d54bb6123090158d
caaker <caaker.0@gmail.com>
2026-03-19 13:00:28 -0500

    update 1

--- F1Footer/F1Footer.jsx
+++ F1Footer/F1Footer.jsx
@@ -11,7 +11,7 @@ export default function F1Footer({config})  {
   return (
     <div id="footer_outer" className={style}>
       <div id="footer_inner" className='footer_style_90'>
-        <a className="footer_item" target="_blank" href={config.github}>github-5</a>
+        <a className="footer_item" target="_blank" href={config.github}>github-1</a>
       </div>
     </div>
   );
