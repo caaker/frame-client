e44f1d0c2510b85cd0cb068cf3996ddebe85a237
caaker <caaker.0@gmail.com>
2026-03-20 09:41:10 -0500

    try 3

--- F1Footer/F1Footer.jsx
+++ F1Footer/F1Footer.jsx
@@ -10,7 +10,7 @@ export default function F1Footer({config})  {
 
   return (
     <div id="footer_outer" className={style}>
-      <a className="footer_item" target="_blank" href={config.github}>github-2</a>
+      <a className="footer_item" target="_blank" href={config.github}>github-3</a>
     </div>
   );
 };
