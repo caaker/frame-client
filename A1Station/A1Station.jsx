3753088ac0d7f93d6f4df7045d96e08424e0c71c
caaker <caaker.0@gmail.com>
2026-03-20 11:12:57 -0500

    improved station

--- A1Station/A1Station.jsx
+++ A1Station/A1Station.jsx
@@ -1,10 +1,9 @@
-import { useEffect, useState }                  from 'react';
-import getLocation                              from './Location';
-import StationClock                             from './StationClock.jsx';
-import StationWeather                           from './StationWeather.jsx';
-import StationAir                               from './StationAir.jsx';
-import                                          './A1Station.css';
-
+import { useEffect, useState } from 'react';
+import getLocation from './Location';
+import StationClock from './StationClock.jsx';
+import StationWeather from './StationWeather.jsx';
+import StationAir from './StationAir.jsx';
+import './A1Station.css';
 
 export default function A1Station() {
   console.logD('DEBUG: L3 : F1-Page-Station ', '#888888');
@@ -13,24 +12,28 @@ export default function A1Station() {
   const [lat, setLat] = useState(30.2827813);
   const [lon, setLong] = useState(-97.7384504);
 
-  getLocation().then((location)=>{
-    setLat(location.coords.latitude);
-    setLong(location.coords.longitude);
-    console.logD('DEBUG: navigator.geolocation: location found ' + location.coords.latitude, '#888888');
-  }).catch((error) =>{
-    console.logD('DEBUG: navigator.geolocation error: no access to location', 'red');
-    console.logD('DEBUG: using default location for Austin - lat - 30.2827813', 'red');
-  });
+  // dependency array is empty, so only run on initial mount
+  useEffect(() => {
+    getLocation()
+      .then((location) => {
+        setLat(location.coords.latitude);
+        setLong(location.coords.longitude);
+        console.logD('DEBUG: navigator.geolocation: location found ' + location.coords.latitude, '#888888');
+      })
+      .catch((error) => {
+        console.logD('DEBUG: navigator.geolocation error: no access to location', 'red');
+      });
+  }, []); 
 
   return (
-    <div className = "page_generic">
+    <div className="page_generic">
       <div id='station'>
         <div id="box_weather">
-            <StationClock/>
-            <StationWeather lat={lat} lon={lon}/>
-            <StationAir lat={lat} lon={lon}/>
+          <StationClock />
+          <StationWeather lat={lat} lon={lon} />
+          <StationAir lat={lat} lon={lon} />
         </div>
       </div>
     </div>
   );
-};
+}
