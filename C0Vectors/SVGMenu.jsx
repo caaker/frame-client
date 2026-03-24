f1380bf9a5537c42509d1f8256444320fb68dc9a
caaker <caaker.0@gmail.com>
2026-03-24 15:38:51 -0500

    reverted to hard code, will fix later

--- C0Vectors/SVGMenu.jsx
+++ C0Vectors/SVGMenu.jsx
@@ -1,6 +1,10 @@
 import ParentSVG from './ParentSVG.jsx';
 
-const path = <path d="M4 8h4V4H4v4zm6 12h4v-4h-4v4zm-6 0h4v-4H4v4zm0-6h4v-4H4v4zm6 0h4v-4h-4v4zm6-10v4h4V4h-4zm-6 4h4V4h-4v4zm6 6h4v-4h-4v4zm0 6h4v-4h-4v4z" />;
-export const SVGMenu = (props) => <ParentSVG id='menu_path1' path={path} {...props} />;
+const path = (
+  <path 
+    id="menu_path1" 
+    d="M4 8h4V4H4v4zm6 12h4v-4h-4v4zm-6 0h4v-4H4v4zm0-6h4v-4H4v4zm6 0h4v-4h-4v4zm6-10v4h4V4h-4zm-6 4h4V4h-4v4zm6 6h4v-4h-4v4zm0 6h4v-4h-4v4z" 
+  />
+);
 
-// what is id=menu_path for ...
+export const SVGMenu = (props) => <ParentSVG path={path} {...props} />;
