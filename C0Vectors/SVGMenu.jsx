82af1d4d268fa75247a20d590baa9ce4b31928ca
caaker <caaker.0@gmail.com>
2026-03-24 14:16:53 -0500

    simplified code for 4 SVGs

--- C0Vectors/SVGMenu.jsx
+++ C0Vectors/SVGMenu.jsx
@@ -1,8 +1,4 @@
 import ParentSVG from './ParentSVG.jsx';
 
-const path = <path id="menu_path" d="M4 8h4V4H4v4zm6 12h4v-4h-4v4zm-6 0h4v-4H4v4zm0-6h4v-4H4v4zm6 0h4v-4h-4v4zm6-10v4h4V4h-4zm-6 4h4V4h-4v4zm6 6h4v-4h-4v4zm0 6h4v-4h-4v4z"></path>;
-export default ( props ) => {
-  return (
-    <ParentSVG path={path} fontSize={props.fontSize} onClick={props.onClick} className={props.className} id={props.id} />
-  );
-};
+const path = <path d="M4 8h4V4H4v4zm6 12h4v-4h-4v4zm-6 0h4v-4H4v4zm0-6h4v-4H4v4zm6 0h4v-4h-4v4zm6-10v4h4V4h-4zm-6 4h4V4h-4v4zm6 6h4v-4h-4v4zm0 6h4v-4h-4v4z" />;
+export const SVGMenu = (props) => <ParentSVG path={path} {...props} />;
