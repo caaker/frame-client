0745092df2c50439e5d96d0b9f2180bd8bb8de45
caaker <caaker.0@gmail.com>
2026-03-24 14:34:17 -0500

    3 down, 2 to go

--- C0Vectors/SVGBookmark.jsx
+++ C0Vectors/SVGBookmark.jsx
@@ -1,9 +1,4 @@
-import React from 'react';
 import ParentSVG from './ParentSVG.jsx';
 
-export default ( props ) => {
-  const path = <path d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2zm0 15l-5-2.18L7 18V5h10v13z"/>;
-  return (
-    <ParentSVG path={path} fontSize={props.fontSize} onClick={props.onClick} className={props.className} id={props.id} />
-  );
-};
+const path = <path d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2zm0 15l-5-2.18L7 18V5h10v13z" />;
+export const SVGBookmark = (props) => <ParentSVG path={path} {...props} />;
--- C0Vectors/SVGCheck.jsx
+++ C0Vectors/SVGCheck.jsx
@@ -1,9 +1,4 @@
-import React from 'react';
 import ParentSVG from './ParentSVG.jsx';
 
-export default ( props ) => {
-  const path = <path d="M9 16.17l-4.88-4.88L2 13l7 7 14-14-2.12-2.12L9 16.17z" />;
-  return (
-    <ParentSVG path={path} fontSize={props.fontSize} onClick={props.onClick} className={props.className} id={props.id} />
-  );
-};
+const path = <path d="M9 16.17l-4.88-4.88L2 13l7 7 14-14-2.12-2.12L9 16.17z" />;
+export const SVGCheck = (props) => <ParentSVG path={path} {...props} />;
