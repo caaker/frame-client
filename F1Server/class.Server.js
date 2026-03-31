c59b05959796affb7bd9fa866490ce64fbbecdeb
caaker <caaker.0@gmail.com>
2026-03-31 23:39:29 +0000

    more

--- F1Server/class.Server.js
+++ F1Server/class.Server.js
@@ -1,22 +1,26 @@
+// update this file to collect data and graph it maybe ...
+// T1 = 01 s (1 second response is considered good or active) ( actual active is about 100 ms )
+// T2 = 60 s (run every 60 seconds to assess server response time) 
+console.logD('DEBUG: L2 : F1-Server');
+
 class Server {
   static logMetric(label, status, startTime) {
     const duration = (performance.now() - startTime).toFixed(2);
     console.logD(`DEBUG: L2 : F1-Server: ${label}: ${status}: ${duration} ms`);
   }
-  static async test(url) {
-    console.logD('DEBUG: L2 : F1-Server');
+  static async testServer(url) {
     const startTime = performance.now();
     try {
       const response = await fetch(url, { signal: AbortSignal.timeout(60000) });
-      this.logMetric('response', response.ok ? 'ok' : '!ok', startTime);
+      this.logMetric('fetch completed', response.ok ? 'ok' : '!ok', startTime);
       return response.ok;
     } catch (error) {
-      const errorMsg = error.name === 'TimeoutError' ? 'timeout' : error.name;
-      this.logMetric('fetch error', errorMsg, startTime);
+      const errorMsg = ( error.name === 'TimeoutError' ) ? 'timeout' : error.name;
+      this.logMetric('fetch errored', errorMsg, startTime);
       return false;
     }
   }
 }
 
-Server.test('https://frame-server-x8qw.onrender.com');
+Server.testServer('https://frame-server-x8qw.onrender.com');
 export default Server;
