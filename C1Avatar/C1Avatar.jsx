56a587a7e34d776a08a5732d97b0ed37ed97a8af
caaker <caaker.0@gmail.com>
2026-04-02 15:32:31 +0000

    refactor of avatar

--- C1Avatar/C1Avatar.jsx
+++ C1Avatar/C1Avatar.jsx
@@ -1,27 +1,14 @@
-import { useDispatch, useSelector }   from    'react-redux';
-import                                        './C1Avatar.css';
+import { useDispatch, useSelector }       from    'react-redux';
+import                                            './C1Avatar.css';
 import { SVGAvatar }                      from    '../C0Vectors/SVGAvatar.jsx';
 
-export default function C1Avatar (props) {
-
+export default function C1Avatar () {
   console.logD('DEBUG: L4 : F1-Apex-Right-Avatar');
-
   const dispatch = useDispatch();
-  const page = useSelector((state) => state.MenuPage.current);
-  const UserCurrent = useSelector((state) => state.User.current);
-
-  function clicked() {
-    dispatch({type: 'updateMenuPage', current: 'User' });
-  }
-
-  const pic = UserCurrent.pic_url;
-  let element;
-  if(pic) {
-    element = <img onClick = {clicked} className = 'user_pic' src = {pic} alt = 'avatar' />;
-  } else {
-    element = <SVGAvatar onClick = {clicked} className = 'user_pic light'/>;
-  }
-
-  return element;
+  const pic = useSelector((state) => state.User.current.pic_url);
+  const clicked = () => dispatch({ type: 'updateMenuPage', current: 'User' });
+  return pic
+    ? <img src={pic} onClick={clicked} className="user_pic" alt="avatar" />
+    : <SVGAvatar fontSize="30px" onClick={clicked} className="user_pic light" />;
 
 };
