fd892a95eb7cd7c38ca04a366f9fe66758fca951
caaker <caaker.0@gmail.com>
2026-04-08 20:22:53 +0000

    updating lfs

--- download_images.js
+++ download_images.js
@@ -2,13 +2,13 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
-const inputPath = path.join("_public-vite", "cache.txt");
+const inputPath = path.join(process.cwd(), "_public-vite", "cache.txt");
 const items = JSON.parse( await fs.readFile(inputPath, "utf8"));
 
 for (const { title, image } of items) {
   try {
     const filename = title.toLowerCase().replace(/[^a-z0-9]+/g, "-") + path.extname(image);
-    const outputPath = path.join("_images-lfs", filename);   
+    const outputPath = path.join(process.cwd(), "_public-vite", "_images-lfs", filename);   
     const res = await fetch(image);
     if (!res.ok) throw new Error(`HTTP ${res.status}`);
     await fs.writeFile(outputPath, res.body);
