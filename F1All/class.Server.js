a5fd06ea33a6dba539f75047d56f45f26f9b2974
caaker <caaker.0@gmail.com>
2026-04-09 15:56:44 +0000

    updated logging

--- F1All/class.Server.js
+++ F1All/class.Server.js
@@ -6,7 +6,7 @@ console.logD('DEBUG: L2 : F1-Server');
 class Server {
   static logMetric(label, status, startTime) {
     const duration = (performance.now() - startTime).toFixed(2);
-    console.logD(`DEBUG: L2 : F1-Server: ${label}: ${status}: ${duration} ms`, 'red');
+    console.logD(`DEBUG: L2 : F1-Server: ${label}: ${status}: ${duration} ms`, 'black');
   }
   static async testServer(url) {
     const startTime = performance.now();
