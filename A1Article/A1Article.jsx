a45303f753bd1f6f6932ba963d30f4f9f23429d5
caaker <caaker.0@gmail.com>
2026-04-09 15:11:44 +0000

    using local image files now

--- A1Article/A1Article.jsx
+++ A1Article/A1Article.jsx
@@ -8,24 +8,16 @@ import                        './A1Article.css';
 export default function A1Article () {
   console.logD('DEBUG: L3 : F1-Page-A1Article : Hello World');
   
-  useEffect(() => {
-    arc.scrollToHash(0);
-  });
+  useEffect(() => arc.scrollToHash(0));
 
   let articles = useSelector((state) => state.Articles.articles);
   let search = useSelector((state) => state.SearchInput.current);
 
+  // created on every render, but OK for now 
   function makeArticles() {
     if (!articles) return null;
-
-    if (search) {
-      articles = articles.filter(val => ArticleFilter(val, search));
-    }
-
-    articles = articles.map((article) => {
-      return <Article key={article._id} article={article}/>;
-    });
-
+    if (search) articles = articles.filter(val => ArticleFilter(val, search));
+    articles = articles.map(article => <Article key={article._id} article={article} />);
     return articles;
   }
 
--- A1Article/Article.jsx
+++ A1Article/Article.jsx
@@ -12,23 +12,25 @@ const handleImageError = (error) => {
 export function Article ({ article }) {
   const { title, link, image, summary, tag, domain } = article;
   const hash = arc.makeAnchorHash(title);
+
+  // move this logic to server and add a hash
+  const extension = new URL(image).pathname.split('.').pop();
+  const location = '/_images-lfs/' + title.toLowerCase().replace(/[^a-z0-9]+/g, "-") + '.' + extension;
+  
+  console.log(location)
+  
   return (
-      <article id={hash} className='__article'>
+    <article id={hash} className='__article'>
+      
       <a className="article_link" href={link} target="_blank" rel="noreferrer">
-        <img className="article_link_image" rel="noreferrer" src={image} alt={title} onError={handleImageError} />
+        <img className="article_link_image" rel="noreferrer" src={location} alt={title} onError={handleImageError} />
       </a>
-      <p className="article_heading">
-        <a className="article_heading_link" href={link} target="_blank">{title}</a>
-      </p>
-      <p className="article_summary">
-        {summary}
-      </p>
-      <p className="article_tag">
-        {tag}
-      </p>
-      <p className="article_domain">
-        {domain}
-      </p>
+
+      <p className="article_heading"> <a className="article_heading_link" href={link} target="_blank">{title}</a> </p>
+      <p className="article_summary"> {summary} </p>
+      <p className="article_tag"> {tag} </p>
+      <p className="article_domain"> {domain} </p>
+      
       <ArticleBar article={article}/>
     </article>
   );
