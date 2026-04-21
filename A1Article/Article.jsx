2b29031c9f061937f778b4ceae95130132a7201a
caaker <caaker.0@gmail.com>
2026-04-21 18:36:37 +0000

    adding flip to articles

--- A1Article/Article.jsx
+++ A1Article/Article.jsx
@@ -5,31 +5,34 @@ import config           from  '../F1All/config_all.js';
 
 const handleImageError = (error) => {
   console.logD('DEBUG: L4 : F1-Page-A1Article-Article : Image not Found ', 'red');
-  console.log('DEBUG:' + error.currentTarget.src);
   error.currentTarget.onerror = null; 
   error.currentTarget.src = config.fallback;   
 };
 
-export function Article ({ article }) {
-  const { title, link, image, summary, tag, domain } = article;
+export function Article ({ article: { title, link, image, summary, tag, domain } }) {
   const hash = arc.makeAnchorHash(title);
-
   const extension = new URL(image).pathname.split('.').pop();
   const location = '/_images-lfs/' + title.toLowerCase().replace(/[^a-z0-9]+/g, "-") + '.' + extension;
   
   return (
-    <article id={hash} className='__article'>
-      
-      <a className="article_link" href={link} target="_blank" rel="noreferrer">
-        <img className="article_link_image" rel="noreferrer" src={location} alt={title} onError={handleImageError} />
-      </a>
 
-      <p className="article_heading"> <a className="article_heading_link" href={link} target="_blank">{title}</a> </p>
-      <p className="article_summary"> {summary} </p>
-      <p className="article_tag"> {tag} </p>
-      <p className="article_domain"> {domain} </p>
-      
-      <ArticleBar article={article}/>
-    </article>
+    <div id='__article-outer'>
+      <div id='__article-inner'>
+        
+        <article id={hash} className='__article'>
+          <a className="article_link" href={link} target="_blank" rel="noreferrer">
+            <img className="article_link_image" src={location} alt={title} onError={handleImageError} />
+          </a>
+          <p className="article_heading"> 
+            <a className="article_heading_link" href={link} target="_blank" rel="noreferrer">{title}</a> 
+          </p>
+          <p className="article_summary"> {summary} </p>
+          <p className="article_tag"> {tag} </p>
+          <p className="article_domain"> {domain} </p>
+          <ArticleBar article={article}/>
+        </article>
+
+      </div>  
+    </div>
   );
 };
