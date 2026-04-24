37dbf0ddd1ede0efe73a0e300abadc940f7212f7
caaker <caaker.0@gmail.com>
2026-04-24 14:42:24 +0000

    deleted _stash

--- _stash/C1Title.jsx
+++
@@ -1,8 +0,0 @@
-import config                         from  '../F1All/config_all.js';
-import                                      './C1Title.css';
-
-export default function C1Title () {
-  return (
-    <p className="apex_title">{config.title}</p>
-  );
-};
--- _stash/PeopleFacesBoxCircle.jsx
+++
@@ -1,9 +0,0 @@
-import './PeopleFacesBoxCircle.css';
-
-export default function PeopleFacesBoxCircle({ src }) {
-  return (
-    <div id="zimage-wrapper">
-      <img id="zimage" src={src}/>
-    </div>
-  );
-};
