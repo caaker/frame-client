4d1dee20d5da446384a8b9b3f5fa1205ffce0c9b
caaker <caaker.0@gmail.com>
2026-05-02 18:59:25 +0000

    fix and refactor

--- F1Modal/Z1Submit.js
+++ F1Modal/Z1Submit.js
@@ -1,5 +1,6 @@
 import Helper                 from '../F1All/class.Helper.js';
 const exp = {};
+const BASE = Helper.getBaseURL();
 
 exp.post = function(state, dispatch) {
   const options = {
@@ -7,7 +8,7 @@ exp.post = function(state, dispatch) {
     method: 'POST',
     body: JSON.stringify(state)
   };
-  Helper.fetchJSON('/articles/add', options, (response) => {
+  Helper.fetchJSON(`${BASE}/articles/add`, options, (response) => {
     dispatch({type: 'addArticle', new_article: response});
   });
 };
@@ -18,7 +19,7 @@ exp.put = function(article_object, dispatch) {
     method: 'PUT',
     body: JSON.stringify(article_object)
   };
-  Helper.fetchJSON(`${Helper.getBaseURL()}/articles/put/${encodeURIComponent(article_object._id)}`, options, (response) => {
+  Helper.fetchJSON(`${BASE}/articles/put/${encodeURIComponent(article_object._id)}`, options, () => {
     dispatch({ type: 'updateArticle', new_article: article_object, index: article_object.index });
   });
 };
