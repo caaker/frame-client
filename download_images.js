e7aa42e73be2c138ffcbc6c9f65cb5b50b5e1fc5
caaker <caaker.0@gmail.com>
2026-08-22 19:28:20 +0000

    enhanced

--- download_images.js
+++ download_images.js
@@ -4,6 +4,7 @@ import path from 'node:path';
 // read the cache.txt and create a JS Obect using JSON.parse
 const inputPath = path.join(process.cwd(), "_public-vite", "cache.txt");
 const items = JSON.parse( await fs.readFile(inputPath, "utf8"));
+
 let ok = true;
 
 // fetch the images in the object and save them to _public-vite/_images-lfs
@@ -13,7 +14,7 @@ for (const { title, image } of items) {
     const outputPath = path.join(process.cwd(), "_public-vite", "_images-lfs", filename);   
     const res = await fetch(image);
     if (!res.ok) throw new Error(`HTTP ${res.status}`);
-    await fs.writeFile(outputPath, res.body);
+    await fs.writeFile(outputPath, Buffer.from(await res.arrayBuffer()));
   } catch (err) {
     ok = false;
     console.error(`Error at: "${title}": ${err.message}`);
