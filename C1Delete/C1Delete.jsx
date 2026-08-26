8ddb8d0753ca176cade3187460654a457b7776a7
caaker <caaker.0@gmail.com>
2026-08-26 18:05:29 +0000

    yes

--- C1Delete/C1Delete.jsx
+++ C1Delete/C1Delete.jsx
@@ -5,7 +5,6 @@ import Helper                       from  '../F1All/class.Helper.js';
 import { deleteArticle }            from '../_redux/a-articles';
 
 export default function C1Delete({ article, admin }) {
-
   const dispatch = useDispatch();
 
   function deleteClicked() {
@@ -14,7 +13,6 @@ export default function C1Delete({ article, admin }) {
     dispatch(deleteArticle(article.index));
     Helper.fetchJSON(`${Helper.getBaseURL()}/articles/delete/${encodeURIComponent(article._id)}`, { method: 'DELETE' });
   }
-
   return <SVGDelete className="article_icons_right" onClick={deleteClicked} />;
 
 }
