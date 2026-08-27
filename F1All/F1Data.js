a32b28e8a635b9b8ccfefe3f9a4bd3912f157524
caaker <caaker.0@gmail.com>
2026-08-27 23:13:25 +0000

    fixed bug in F1Data

--- F1All/F1Data.js
+++ F1All/F1Data.js
@@ -8,16 +8,17 @@ import { getArticles, saveArticles } from   '../F1LS/F1LSArticles.js';
   console.logD('DEBUG: L2 : F1-Data', '#34A853');
   const urls = Helper.getURLs(true);
 
+  // get Articles from localStorage or fetch them
   const articles = getArticles();
   if(articles && articles.length > 0) {
     store.dispatch(initializeArticles(articles));
-    return;
+  } else {
+    Helper.fetchJSON(urls.articles, undefined, (arts) => {
+      saveArticles(arts) 
+      store.dispatch(initializeArticles(arts));
+    });
   } 
 
-  Helper.fetchJSON(urls.articles, undefined, (arts) => {
-    saveArticles(arts) 
-    store.dispatch(initializeArticles(arts));
-  });
-
   Helper.fetchJSON(urls.users, undefined, (user) => { store.dispatch(initializeUser(user)); });
+
 })();
